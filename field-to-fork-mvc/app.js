04a512d4eede0431c3b64100bf6b9635f7ae4b3e
Zaksk <zakariamar.99@gmail.com>
2025-02-25 11:58:21 +0000

    Add app.js

---
+++ field-to-fork-mvc/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+
+//const userRouter = require('./routes/user')
+//const questionsRouter = require('./routes/products')
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        name: "Field To Fork",
+        description: "A platform dedicated to supporting local sustainability efforts by reducing food waste and providing affordable, fresh produce to the community."
+    })
+})
+
+//app.use("/users", userRouter);
+//app.use("/products", questionsRouter)
+
+module.exports = app;
