573f504b7689887e4f95dea8a5818bc6c6970523
Mary <haritmash@yandex.ru>
2025-02-26 13:28:06 +0000

    Add product types to the db, fix minor bug in Users model, update the Products model

--- field-to-fork-mvc/db/db.sql
+++ field-to-fork-mvc/db/db.sql
@@ -1,13 +1,14 @@
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS categories;
-DROP TABLE IF EXISTS price_types;
-DROP TABLE IF EXISTS types;
-DROP TABLE IF EXISTS products;
-DROP TABLE IF EXISTS comments;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS categories CASCADE;
+DROP TABLE IF EXISTS price_types CASCADE;
+DROP TABLE IF EXISTS types CASCADE;
+DROP TABLE IF EXISTS products CASCADE;
+DROP TABLE IF EXISTS comments CASCADE;
 
 CREATE TABLE users (
     user_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
+    username VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL,
     postcode VARCHAR(50) NOT NULL,
     password_hash VARCHAR(200),
@@ -63,3 +64,63 @@ VALUES
 ('cut flowers'),
 ('other');
 
+-- Adding fruits
+INSERT INTO types (category_id, price_type_id, type_name) VALUES
+(1, 1, 'apples'),
+(1, 1, 'pears'),
+(1, 1, 'raspberries'),
+(1, 1, 'strawberries'),
+(1, 1, 'blackberries'),
+(1, 1, 'currants'),
+(1, 1, 'blueberries'),
+(1, 1, 'plums'),
+(1, 1, 'cherries'),
+(1, 1, 'gooseberries');
+
+-- Adding vegetables
+INSERT INTO types (category_id, price_type_id, type_name) VALUES
+(2, 1, 'spring_greens'),
+(2, 1, 'carrots'),
+(2, 2, 'cauliflower'),
+(2, 1, 'celeriac'),
+(2, 1, 'cucumbers'),
+(2, 1, 'leeks'),
+(2, 2, 'lettuce'),
+(2, 1, 'onion'),
+(2, 1, 'swede'),
+(2, 1, 'turnip'),
+(2, 1, 'parsnips'),
+(2, 1, 'rhubarb'),
+(2, 1, 'capsicum'),
+(2, 1, 'chinese_leaf'),
+(2, 1, 'celery'),
+(2, 1, 'tomatoes'),
+(2, 1, 'coriander'),
+(2, 1, 'spinach_leaf'),
+(2, 1, 'calabrese'),
+(2, 1, 'rocket'),
+(2, 1, 'mixed_babyleaf_salad'),
+(2, 2, 'sweetcorn'),
+(2, 1, 'beans'),
+(2, 1, 'courgettes'),
+(2, 1, 'peas'),
+(2, 1, 'asparagus'),
+(2, 1, 'watercress');
+
+-- Adding the pot plants 
+INSERT INTO types (category_id, price_type_id, type_name) VALUES
+(3, 5, 'cyclamen'),
+(3, 5, 'poinsettia'),
+(3, 5, 'geranium');
+
+-- Adding the cut flowers
+INSERT INTO types (category_id, price_type_id, type_name) VALUES
+(4, 3, 'tulips'),
+(4, 3, 'gladioli'),
+(4, 3, 'alstromeria'),
+(4, 3, 'lillies'),
+(4, 3, 'narcissus'),
+(4, 3, 'chrysanthemum'),
+(4, 3, 'stocks'),
+(4, 3, 'sweet_williams'),
+(4, 3, 'peony');
