cc4d5761d1bd28bd389da0bd9ba4a43689a394dd
Zaksk <zakariamar.99@gmail.com>
2025-02-26 10:21:26 +0000

    Fix issues

--- field-to-fork-mvc/models/User.js
+++ field-to-fork-mvc/models/User.js
@@ -1,48 +1,53 @@
-const db = require('../database/connect');
+const db = require('../db/connect'); // Updated to match your 'db' folder
 
 class User {
-
-    constructor({ user_id, name, username, email, postcode, password, crated_at}) {
+    constructor({ user_id, name, username, email, postcode, password, created_at }) {
         this.user_id = user_id;
         this.name = name;
-        this.password = password;
-        this.crated_at = crated_at;
-        this.postcode = postcode;
-        this.email = email;
         this.username = username;
+        this.email = email;
+        this.postcode = postcode;
+        this.password = password;
+        this.created_at = created_at; // Fixed typo from 'crated_at'
     }
 
     static async getOneById(id) {
-        const response = await db.query("SELECT  user_id, name, email, postcode, password, crated_at FROM Users WHERE mary_id = $1", [id]);
-        if (response.rows.length != 1) {
+        const response = await db.query(
+            "SELECT user_id, name, username, email, postcode, password, created_at FROM Users WHERE user_id = $1",
+            [id]
+        );
+        if (response.rows.length !== 1) {
             throw new Error("Unable to locate user.");
         }
         return new User(response.rows[0]);
     }
 
-
-
     static async getOneByUsername(username) {
-        const response = await db.query("SELECT user_id, username, password FROM Users WHERE username = $1", [username]);
-        if (response.rows.length != 1) {
+        const response = await db.query(
+            "SELECT user_id, username, password FROM Users WHERE username = $1",
+            [username]
+        );
+        if (response.rows.length !== 1) {
             throw new Error("Unable to locate user.");
         }
         return new User(response.rows[0]);
     }
 
     static async create(data) {
-        const { name, surname, username, password, email } = data;
-        if(username == undefined || password == undefined) throw Error("Ensure username and password are both provided")
-        let response = await db.query("INSERT INTO Users ( user_id, name, email, postcode, password) VALUES ($1, $2, $3, $4, $5) RETURNING user_id;",
-            [ user_id, name, email, postcode, password]);
-        const newId = response.rows[0].login_id;
-        const newUser = await User.getOneById(newId);
-        return newUser;
+        const { name, username, email, postcode, password } = data;
+
+        if (!username || !password) {
+            throw new Error("Ensure username and password are both provided");
+        }
+
+        const response = await db.query(
+            "INSERT INTO Users (name, username, email, postcode, password) VALUES ($1, $2, $3, $4, $5) RETURNING user_id;",
+            [name, username, email, postcode, password]
+        );
+
+        const newId = response.rows[0].user_id;
+        return await User.getOneById(newId);
     }
-
-
-
-
 }
 
 module.exports = User;
