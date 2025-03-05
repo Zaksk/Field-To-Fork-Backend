4324dd351d6fdbc71df01486d10eaf787f4bf6af
Zaksk <zakariamar.99@gmail.com>
2025-03-05 23:14:48 +0000

    Edit to routers

--- field-to-fork-mvc/routers/s3Routes.js
+++ field-to-fork-mvc/routers/s3Routes.js
@@ -1,8 +1,11 @@
 const { Router } = require("express");
 const AWS = require("aws-sdk");
 require("dotenv").config();
+const cors = require("cors");  
 
 const s3Router = Router();
+
+s3Router.use(cors()); 
 
 const s3 = new AWS.S3({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
