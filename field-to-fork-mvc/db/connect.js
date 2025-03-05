898bb3fd4228f1b8db502a2d9717ef5e16fa5605
Zaksk <zakariamar.99@gmail.com>
2025-03-05 21:23:45 +0000

    Change connect.js for testing purposes

--- field-to-fork-mvc/db/connect.js
+++ field-to-fork-mvc/db/connect.js
@@ -1,21 +1,21 @@
 
 const { Pool } = require("pg");
 
-// Version for db as a microservice
-const db = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT || 5432,
-});
+// // Version for db as a microservice
+// const db = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT || 5432,
+// });
 
 // Version of url_db for solid service
-/*
+
 const db = new Pool({
     connectionString: process.env.DB_URL
 })
-*/
+
 
 console.log("DB connection established.")
 
